7ed06dfebe7c39fd5a6b718c38cfa2488d764e0a
Victoria Bulakh <bulakh.victoria29@gmail.com>
2022-10-06 22:18:54 +0300

    Add NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,15 +1,17 @@
 import { useNavigate } from 'react-router-dom';
+import { cssTransition } from 'react-toastify';
+import css from '/NotFound.module.css'
 
 export default function NotFound() {
   const history = useNavigate();
 
-  const onGoBack = () => {
-    history.push('/');
+  const onReturn = () => {
+    history('/');
   };
   return (
     <>
       <h2>404 Not Found </h2>
-      <button type="button" onClick={onGoBack}>
+      <button type="button" onClick={onReturn} className={css.button}>
         Go home
       </button>
     </>
